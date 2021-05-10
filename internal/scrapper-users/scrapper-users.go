47904d23ed084205618893fbb3caeb4421e9f9da
adefirmanf <syskeys32@gmail.com>
2021-05-11 00:58:14 +0700

    add scrapper-users

--- internal/scrapper-users/scrapper-users.go
+++ internal/scrapper-users/scrapper-users.go
@@ -1,8 +1,39 @@
 package scrapperusers
 
-import "github.com/adefirmanf/data_selection_pretexting/internal/storage"
+import (
+	"net/http"
+
+	"github.com/adefirmanf/data_selection_pretexting/internal/queue"
+	"github.com/adefirmanf/data_selection_pretexting/internal/storage"
+)
 
 // ScrapperUsers .
 type ScrapperUsers struct {
-	Storage storage.Storage
+	HTTPClient *http.Client
+	Storage    *storage.Storage
+	Queue      *queue.Queue
+}
+
+// FetchFollowings .
+func (s *ScrapperUsers) FetchFollowings(UserID string) {
+
+}
+
+// FetchFollowers .
+func (s *ScrapperUsers) FetchFollowers(UserID string) {
+
+}
+
+// FetchLookup .
+func (s *ScrapperUsers) FetchLookup(UserID string) {
+
+}
+
+// NewScrapperUser .
+func NewScrapperUser(httpClient *http.Client, storage *storage.Storage, queue *queue.Queue) *ScrapperUsers {
+	return &ScrapperUsers{
+		HTTPClient: httpClient,
+		Storage:    storage,
+		Queue:      queue,
+	}
 }
